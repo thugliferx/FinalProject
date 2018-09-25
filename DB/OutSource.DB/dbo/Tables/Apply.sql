fd8d1d0d4f2a05878930c4e4b54f29ea187e210d
caryhaha <42676148+caryhaha@users.noreply.github.com>
2018-09-25 15:27:28 +0800

    DBupdate

--- DB/OutSource.DB/dbo/Tables/Apply.sql
+++ DB/OutSource.DB/dbo/Tables/Apply.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Apply]
 (
 	[CaseId] INT NOT NULL,
-	[RecId] INT NOT NULL,
+	[Id] INT NOT NULL,
 	[State] NVARCHAR (256) NOT NULL,
-	CONSTRAINT [PK_Apply] PRIMARY KEY NONCLUSTERED ([CaseId],[RecId]),  
-	CONSTRAINT [FK_Case_Apply_CaseId] FOREIGN KEY  ([CaseId])  REFERENCES [dbo].[Case] ([Id])   ON DELETE CASCADE,  
-	CONSTRAINT [FK_Receiver_Apply_RecId] FOREIGN KEY  ([RecId])  REFERENCES [dbo].[Receiver] ([RecId])   ON DELETE CASCADE    
+	[Evalution] VARCHAR (64),
+	[Comment] NVARCHAR (256),
+	CONSTRAINT [PK_Apply] PRIMARY KEY NONCLUSTERED ([CaseId],[Id]),  
+	CONSTRAINT [FK_Case_Apply_CaseId] FOREIGN KEY  ([CaseId])  REFERENCES [dbo].[Case] ([CaseId])   ON DELETE CASCADE,  
+	CONSTRAINT [FK_Receiver_Apply_RecId] FOREIGN KEY  ([Id])  REFERENCES [dbo].[Receiver] ([Id])   ON DELETE CASCADE    
 
 )
