ca79cd3003273cbe9241f15ee42c7d10fa684d07
thugliferx <andy6100988@gmail.com>
2018-10-09 14:53:14 +0800

    Email
    Email

--- WebApplication1/Controllers/JquryController.cs
+++ WebApplication1/Controllers/JquryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using WebApplication1.Models;
 
@@ -17,17 +18,21 @@ namespace WebApplication1.Controllers
             }
             public JsonResult SaveData(User model)
             {
-                db.Users.Add(model);
-            try
-            {
+            model.Isvalid=false;
+            db.Users.Add(model);
+            
                 db.SaveChanges();
-            }
-            catch (System.Data.Entity.Validation.DbEntityValidationException ex)
-            {
-                throw ex;
-            }
+           
+            BuildEmailTemplate(model.Id);
+           
             return Json("Registration Successful" , JsonRequestBehavior .AllowGet);
             }
+
+        public void BuildEmailTemplate(string Regid)
+        {
+            string body = System.IO.File.ReadAllText(HostingEnvironment.MapPath("~/EmailTemplate/")+"Text"+".cshtml");
+        }
+
         
     }
 }
