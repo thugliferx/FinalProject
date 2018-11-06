32059420707f348525e30517be867cda737ff52f
caryhaha <42676148+caryhaha@users.noreply.github.com>
2018-11-06 13:47:55 +0800

    DB UPDATE

--- DB/OutSource.DB/dbo/Tables/Working.sql
+++ DB/OutSource.DB/dbo/Tables/Working.sql
@@ -3,8 +3,10 @@
 	[CaseId] INT NOT NULL ,
 	[Id] INT NOT NULL ,
 	[Done] NVARCHAR (16) NOT NULL,
-	[Asses] NVARCHAR (64)  NULL,
-	[Comment] NVARCHAR (64) NULL, 
+	[Creater's_Asses] NVARCHAR (64)  NULL,
+	[Creater's_Comment] NVARCHAR (64) NULL,
+	[Receiver's_Asses] NVARCHAR (64)  NULL,
+	[Receiver's_Comment] NVARCHAR (64) NULL, 
 	CONSTRAINT [PK_Working] PRIMARY KEY CLUSTERED 
 	(
 	[CaseId],
